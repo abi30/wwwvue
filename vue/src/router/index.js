2a9e1cc753e39f17bbcaf027aecdf691c9c3be4f
abi30 <rakib0751@gmail.com>
2022-12-13 14:44:11 +0100

    till 1.43.0

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -5,6 +5,7 @@ import GuestLayout from "../components/GuestLayout.vue";
 import MealsByName from "../views/MealsByName.vue";
 import MealsByLetter from "../views/MealsByLetter.vue";
 import MealsByIngredient from "../views/MealsByIngredient.vue";
+import MealDetails from "../views/MealDetails.vue";
 
 const routes = [
   {
@@ -31,6 +32,11 @@ const routes = [
         name: "byLetter",
         component: MealsByLetter,
       },
+      {
+        path: "/meal/:id",
+        name : 'mealDetails',
+        component: MealDetails,
+      }
     ],
   },
   {
